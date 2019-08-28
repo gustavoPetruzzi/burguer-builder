7fce7b56d0d25567115c39462c9c66ced20163f2
gustavo <gustavopetruzzi19@gmail.com>
2019-08-28 00:28:31 +0000

    muestra hamburguesa

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
+class BurgerBuilder extends Component {
+
+    state ={
+        ingredients:{
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 2
+        }
+    }
+    render(){
+        return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients}></Burger>
+                <div> Build controls</div>
+                
+            </Aux>
+        )
+    }
+}
+
+
+
+export default BurgerBuilder;
