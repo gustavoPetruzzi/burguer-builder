a7e734bb7464c69418998e5347e74c7179085351
Gustavo <gustavopetruzzi19@gmail.com>
2019-11-13 16:56:14 -0300

    agrego

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Layout from './hoc/Layout/Layout';
 import './App.css';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Checkout/Orders/Orders';
 import { BrowserRouter,Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <Layout>
+    
       <BrowserRouter>
-        <Switch >
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-        </Switch>
+        <Layout>
+          <Switch >
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} /> 
+          </Switch>
+        </Layout>
       </BrowserRouter>
-    </Layout>
+    
   );
 }
 
