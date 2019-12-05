c276a2e58633affdc6652672b6e0303081d8cc21
Gustavo <gustavopetruzzi19@gmail.com>
2019-12-05 16:48:02 -0300

    agrego middleware

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
 
+
+const logger = store =>{
+    return next =>{
+        return action =>{
+            console.log('[Middleware] Dispatching ',action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+const store = createStore(reducer, applyMiddleware(logger));
 const app = (
     <Provider store={store}>
         <App />
     </Provider>
 )
+
 
 ReactDOM.render(app, document.getElementById('root'));
 
